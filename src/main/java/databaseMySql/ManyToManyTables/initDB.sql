f3af2c5c6893b1466a2f2d91312b8ee5272d7ce8
SlivkaEvgen <79520449+SlivkaEvgen@users.noreply.github.com>
2021-07-12 19:06:58 +0300

    Update initDB.sql

--- src/main/java/databaseMySql/ManyToManyTables/initDB.sql
+++ src/main/java/databaseMySql/ManyToManyTables/initDB.sql
@@ -1,11 +1,5 @@
 USE MySqlGoItHomeWork;
 
-# DROP TABLE IF EXISTS developerProjectRelation;
-# DROP TABLE IF EXISTS skills;
-# DROP TABLE IF EXISTS developers;
-# DROP TABLE IF EXISTS projects;
-# DROP TABLE IF EXISTS customers;
-# DROP TABLE IF EXISTS companies;
 
 CREATE TABLE companies
 (
@@ -55,6 +49,7 @@ CREATE TABLE projects
     FOREIGN KEY (id) REFERENCES companies (Id) ON DELETE CASCADE,
     FOREIGN KEY (id) REFERENCES customers (Id) ON DELETE CASCADE
 );
+
 CREATE TABLE developers_projects
 (
     developer_id int NOT NULL,
@@ -81,56 +76,4 @@ CREATE TABLE developers_skills
     FOREIGN KEY (developer_id) REFERENCES developers (id),
     FOREIGN KEY (skills_id) REFERENCES skills (id)
 );
-
-# ALTER TABLE skills
-#     ADD CONSTRAINT developer_id FOREIGN KEY (id)
-#         REFERENCES skills (id) ON DELETE CASCADE;
-#     company_id  INT                    NOT NULL
-#     FOREIGN KEY (company_id) REFERENCES companies (Id)
-# FOREIGN KEY (ProductId) REFERENCES Products(Id) ON DELETE CASCADE,
-#     FOREIGN KEY (CustomerId) REFERENCES Customers(Id) ON DELETE CASCADE
-#     CONSTRAINT skills_id PRIMARY KEY (id),
-#     FOREIGN KEY (id) REFERENCES developers (id) ON DELETE CASCADE
-#     CONSTRAINT skills_id FOREIGN KEY skills (id)
-#         REFERENCES developers (id)
-# create table developerProjectRelation
-# (
-#     developer_id int not null,
-#     project_id   int not null,
-#     constraint developer_id
-#         unique (developer_id, project_id),
-#     constraint developerProjectRelation_ibfk_1
-#         foreign key (developer_id) references developers (id),
-#     constraint developersProjectRelation_ibfk_2
-#         foreign key (project_id) references projects (id)
-# );
-# ALTER TABLE skills
-#     ADD CONSTRAINT developer_id FOREIGN KEY developers(developer_id)
-#         REFERENCES skills (id) ON DELETE CASCADE;
-#
-# ALTER TABLE developers
-#     ADD CONSTRAINT developer_id FOREIGN KEY (company_id)
-#         REFERENCES developers (id) ON DELETE CASCADE;
-#
-# ALTER TABLE projects
-#     ADD CONSTRAINT projects_id FOREIGN KEY (company_id)
-#         REFERENCES companies (id);
-#
-# ALTER TABLE projects
-#     ADD CONSTRAINT customer_id FOREIGN KEY (customer_id)
-#         REFERENCES customers (id);
-#
-# ALTER TABLE customers
-#     ADD CONSTRAINT company_id FOREIGN KEY (company_id)
-#         REFERENCES companies (id);
-
-# create index project_id
-#     on developerProjectRelation (project_id);
-
-
-# ALTER TABLE skills
-#     ADD CONSTRAINT developerSkills
-#         FOREIGN KEY (id)
-#             REFERENCES developers (id);
-#
 
