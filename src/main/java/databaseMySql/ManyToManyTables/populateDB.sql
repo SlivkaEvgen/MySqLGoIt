acf8c5e84836eddbe318d36037165f1e1d9042b5
SlivkaEvgen <79520449+SlivkaEvgen@users.noreply.github.com>
2021-07-12 20:36:30 +0300

    Update populateDB.sql

--- src/main/java/databaseMySql/ManyToManyTables/populateDB.sql
+++ src/main/java/databaseMySql/ManyToManyTables/populateDB.sql
@@ -4,7 +4,6 @@ INSERT INTO companies(id,name)
 VALUES (1,'GoIT'),
        (2,'StarsIT'),
        (3,'UpIT');
-#        ('NewIT',3,3,3),
 
 INSERT INTO customers(id,name, budget,company_id)
 VALUES (1,'RichMan1', 100000,1),
@@ -15,8 +14,6 @@ INSERT INTO developers(id, name, age, gender, email, numberPhone,company_id)
 VALUES (1, 'Вася', 35, 'Male', 'abc@com.ua', 8765431,1),
        (2, 'Колян', 25, 'Male', 'abc1@com.ua', 8765432,2),
        (3, 'Колян1', 26, 'Male', 'abc2@com.ua', 8765433,3);
-#        (4, 'Колян2', 27, 'Male', 'abc3@com.ua', 8765434,1),
-#        (5, 'Юра', 20, 'Male', 'abc4@com.ua', 8765435,2);
 
 INSERT INTO skills(id,activities, level)
 VALUES (1,'Java', 'Senior'),
@@ -37,8 +34,7 @@ INSERT INTO projects(id,name, deadlines_days,company_id,customer_id)
 VALUES (1,'Bot', 45,1,1),
        (2,'Car', 70,2,2),
        (3,'School',55,2,2);
-#        (3,'GoItProduct',3),
-#        (5,'GoItSchool',5),
+
 INSERT INTO developers_skills(developer_id, skills_id)
 VALUES (1,1),
        (2,5),
@@ -53,8 +49,3 @@ INSERT INTO developers_projects(developer_id, project_id)
 VALUES (1,1),
        (2,2),
        (3,3);
-
-
-
-
-
